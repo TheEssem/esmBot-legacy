bbcc3f5d7705be33d379f2921d568682759e6ac6
TheEssemCraft <smswessem@gmail.com>
2018-05-21 15:10:29 +0000

    Added &mail and modified &magik

--- commands/magik.js
+++ commands/magik.js
@@ -1,20 +1,24 @@
 const request = require("request");
+const tempy = require("tempy");
 const gm = require("gm").subClass({
   imageMagick: true
 });
 
 exports.run = async (client, message, args) => { // eslint-disable-line no-unused-vars
   const image = client.getImage(message);
+  const magikTemp = tempy.file({extension: "png"});
+  const magikOutput = tempy.file({extension: "png"});
   if (image !== undefined) {
     const processMessage = await message.channel.send("⚙️ Processing... This might take a while");
     gm(request(image)).size((error, size) => {
       if (error) throw new Error(error);
-      gm(request(image)).out("-liquid-rescale", `${size.width * 0.5}x${size.height * 0.5}`).strip().stream((error, stdout) => {
+      gm(request(image)).out("-liquid-rescale", `${size.width * 0.5}x${size.height * 0.5}`).strip().write(magikTemp, (error) => {
         if (error) throw new Error(error);
-        gm(stdout).out("-liquid-rescale", `${size.width * 1.5}x${size.height * 1.5}`).strip().stream(async (error, stdout) => {
+        gm(magikTemp).out("-liquid-rescale", `${size.width * 1.5}x${size.height * 1.5}`).strip().write(magikOutput, async (error) => {
+          if (error) throw new Error(error);
           await message.channel.send({
             files: [{
-              attachment: stdout,
+              attachment: magikOutput,
               name: "magik.png"
             }]
           });
