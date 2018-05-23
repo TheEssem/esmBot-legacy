648ff1ebba5fa04d07a09a7b5c506f5626cc9972
TheEssemCraft <smswessem@gmail.com>
2018-05-23 00:56:05 +0000

    Added &deepfry

---
+++ commands/deepfry.js
@@ -0,0 +1,23 @@
+const request = require("request");
+const gm = require("gm").subClass({
+  imageMagick: true
+});
+
+exports.run = async (client, message, args) => { // eslint-disable-line no-unused-vars
+  const image = client.getImage(message);
+  if (image !== undefined) {
+    message.channel.startTyping();
+    gm(request(image)).colorspace("RGB").modulate(200, 500, 105).contrast(-5).setFormat("jpg").quality(1).stream((error, stdout) => {
+      if (error) throw new Error(error);
+      message.channel.stopTyping();
+      message.channel.send({
+        files: [{
+          attachment: stdout,
+          name: "deepfry.jpg"
+        }]
+      });
+    });
+  } else {
+    message.reply("you need to provide a PNG or JPEG file to deep fry!");
+  }
+};
