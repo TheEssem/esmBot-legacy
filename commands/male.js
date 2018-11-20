ab5a4e19e3b6712fb206ea1211d77ed8bc4f1eb0
TheEssemCraft <smswessem@gmail.com>
2018-11-20 15:18:01 -0600

    Added face manipulation commands (smile, beard, hot, etc.)

---
+++ commands/male.js
@@ -0,0 +1,26 @@
+const request = require("request-promise-native");
+const faceapp = require("faceapp");
+
+exports.run = async (client, message, args) => { // eslint-disable-line no-unused-vars
+  const image = await client.getImage(message).catch(error => {
+    message.reply("you need to provide an image with a face to make it look male!");
+    console.log(error);
+  });
+  if (image !== undefined) {
+    message.channel.startTyping();
+    const downloadedImage = await request({ uri: image, encoding: null });
+    const faceImage = await faceapp.process(downloadedImage, "male").catch(error => {
+      console.log(error);
+      return message.reply("I couldn't find a face!");
+    });
+    message.channel.stopTyping();
+    message.channel.send({
+      files: [{
+        attachment: faceImage,
+        name: "male.png"
+      }]
+    });
+  }
+};
+
+exports.aliases = [];
