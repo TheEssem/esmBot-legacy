bb6352107f78d052750d2eea2c21a306eb11e186
TheEssemCraft <smswessem@gmail.com>
2019-04-11 09:54:15 -0500

    Added trump, fixed bug with scott's aliases

--- commands/trump.js
+++ commands/trump.js
@@ -7,22 +7,22 @@ const gm = require("@tohru/gm").subClass({
 
 exports.run = async (client, message, args) => { // eslint-disable-line no-unused-vars
   const image = await client.getImage(message).catch(error => {
-    message.reply("you need to provide an image to make a Scott the Woz TV meme!");
+    message.reply("you need to provide an image to make a Trump meme!");
     console.log(error);
   });
   const imageFile = tempy.file({extension: "png"});
   if (image !== undefined) {
     message.channel.startTyping();
-    const template = "./assets/images/scott.png";
+    const template = "./assets/images/trump.png";
     const downloadImage = request.get(image).pipe(fs.createWriteStream(imageFile));
     downloadImage.on("finish", () => {
-      gm(template).out("-gravity").out("Center").out("(").out(imageFile).out("-virtual-pixel").out("transparent").out("-resize").out("415x234!").out("+distort").out("Perspective").out("0,0 129,187 415,0 517,182 415,234 517,465 0,234 132,418").out("-geometry").out("-110+83").out(")").out("-composite").stream((error, stdout) => {
+      gm(template).out("-background").out("none").out("-gravity").out("South").out("(").out("-clone").out("0").out("(").out(imageFile).out("-virtual-pixel").out("transparent").out("-resize").out("365x179!").out("+distort").out("Perspective").out("0,0 207,268 365,0 548,271 365,179 558,450 0,179 193,450").out(")").out("-geometry").out("-25-1").out("-composite").out(")").out("+swap").out("-composite").stream((error, stdout) => {
         if (error) throw new Error(error);
         message.channel.stopTyping();
         message.channel.send({
           files: [{
             attachment: stdout,
-            name: "scott.png"
+            name: "trump.png"
           }]
         });
       });
@@ -30,4 +30,4 @@ exports.run = async (client, message, args) => { // eslint-disable-line no-unuse
   }
 };
 
-exports.aliases = ["woz", "tv", "porn"];
+exports.aliases = [];
